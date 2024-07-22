2ffa2be87c9c56ea7da35cc3a3e548af66bfa697
jordn24 <45545394+jordn24@users.noreply.github.com>
2024-07-22 22:21:38 +1000

    Changed mobile text size

--- frontend/src/components/MobileScreen.tsx
+++ frontend/src/components/MobileScreen.tsx
@@ -5,8 +5,8 @@ const MobileScreen: React.FC = () => {
     return (
         <Grid item width={'100%'} border={1} bgcolor={'white'} margin={12}>
             <Box display={'flex'} flexDirection={'column'} alignItems={'center'} color={'green'}>
-                <Typography variant="h5" textAlign={'center'} margin={5}>This website doesn't work on small screens!</Typography>
-                <Typography variant="h5" textAlign={'center'} margin={5}>Try it out on a computer</Typography>
+                <Typography variant="h6" textAlign={'center'} margin={5}>This website doesn't work on small screens!</Typography>
+                <Typography variant="h6" textAlign={'center'} margin={5}>Try it out on a computer</Typography>
             </Box>
         </Grid>
     )
